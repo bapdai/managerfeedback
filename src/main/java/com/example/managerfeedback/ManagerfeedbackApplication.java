62012e6c0df98128362c1daf59daeadcc5e08c9d
bapdai <97323194+bapdai@users.noreply.github.com>
2022-07-19 14:38:54 +0700

    make it better

--- src/main/java/com/example/managerfeedback/ManagerfeedbackApplication.java
+++ src/main/java/com/example/managerfeedback/ManagerfeedbackApplication.java
@@ -1,10 +1,8 @@
 package com.example.managerfeedback;
 
-import com.example.managerfeedback.service.NewsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.domain.Pageable;
+
 
 @SpringBootApplication
 public class ManagerfeedbackApplication {
